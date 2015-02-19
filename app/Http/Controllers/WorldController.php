d019749b43bcda22159daa3808128452ab39a63e
Lily Belle <tolilybelle@gmail.com>
2015-02-19 21:47:17 +0100

    Add controller for worlds

---
+++ app/Http/Controllers/WorldController.php
@@ -0,0 +1,12 @@
+<?php namespace Rpgo\Http\Controllers;
+
+use Rpgo\Http\Requests;
+use Rpgo\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class WorldController extends Controller {
+
+	//
+
+}
