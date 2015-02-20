ff159d925003bca3d9a1446ae75afed52fd00b2a
Lily Belle <tolilybelle@gmail.com>
2015-02-20 06:41:57 +0100

    Make Member model use uuid

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model {
 
+    public $incrementing = false;
+
     public function user()
     {
         return $this->belongsTo(User::class);
