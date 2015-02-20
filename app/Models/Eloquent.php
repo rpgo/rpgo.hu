cb6f18d15ad51fc7a3f0a26e8d2e6324842d0dc0
Lily Belle <tolilybelle@gmail.com>
2015-02-20 03:18:52 +0100

    Make models generate uuid if not incrementing id

--- app/Models/Eloquent.php
+++ app/Models/Eloquent.php
@@ -1,9 +1,31 @@
 <?php namespace Rpgo\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Rhumsaa\Uuid\Uuid;
 
 class Eloquent extends Model {
 
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
 
+        /**
+         * Use UUID if not incrementing id.
+         */
+        static::creating(function (Eloquent $model) {
+            if ($model->incrementing)
+                return;
+
+            $key = $model->getKeyName();
+
+            if (empty($model->$key))
+                $model->$key = (string) Uuid::uuid4();
+        });
+    }
 
 }
