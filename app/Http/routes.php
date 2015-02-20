846f7b583ee88c8df19215d29f4b0c027b44c167
Lily Belle <tolilybelle@gmail.com>
2015-02-20 19:00:06 +0100

    Add routes for members

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,8 +14,32 @@
 Route::group(['domain' => '{world}.' . env('APP_DOMAIN')], function(){
 
     Route::get('/', [
-        'uses'      => 'WorldController@main',
-        'as'        => 'world.main',
+        'uses'          => 'WorldController@main',
+        'as'            => 'world.main',
+    ]);
+
+    Route::get(trans('routes.member.create'), [
+        'uses'          => 'MemberController@create',
+        'as'            => 'member.create',
+        'middleware'    => 'auth',
+    ]);
+
+    Route::post(trans('routes.member.store'), [
+        'uses'          => 'MemberController@store',
+        'as'            => 'member.store',
+        'middleware'    => 'auth',
+    ]);
+
+    Route::get(trans('routes.member.index'), [
+        'uses'          => 'MemberController@index',
+        'as'            => 'member.index',
+        'middleware'    => 'auth',
+    ]);
+
+    Route::get(trans('routes.member.show', ['parameter' => '{member}']), [
+        'uses'          => 'MemberController@show',
+        'as'            => 'member.show',
+        'middleware'    => 'auth',
     ]);
 
 });
@@ -27,8 +51,8 @@ Route::group(['domain' => env('APP_DOMAIN')], function(){
     Route::get('home', 'HomeController@index');
 
     Route::controllers([
-        'auth' => 'Auth\AuthController',
-        'password' => 'Auth\PasswordController',
+        'auth'          => 'Auth\AuthController',
+        'password'      => 'Auth\PasswordController',
     ]);
 
     Route::get(trans('routes.world.create'), [
