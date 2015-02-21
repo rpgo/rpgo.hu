3b7799a120b05f6f4ed1800392de3c3ac3d921f0
Lily Belle <tolilybelle@gmail.com>
2015-02-21 08:51:11 +0100

    Refactor user menu in navigation

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -24,15 +24,11 @@
 
             <ul class="nav navbar-nav navbar-right">
                 @if (! $user )
-                    <li><a href="/auth/login">Login</a></li>
-                    <li><a href="/auth/register">Register</a></li>
+                    @include('common.nav.guest')
+                @elseif (! $member)
+                    @include('common.nav.user')
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ $user->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="/auth/logout">Logout</a></li>
-                        </ul>
-                    </li>
+                    @include('common.nav.member')
                 @endif
             </ul>
         </div>
---
+++ resources/views/common/nav/guest.blade.php
@@ -0,0 +1,2 @@
+<li><a href="/auth/login">Login</a></li>
+<li><a href="/auth/register">Register</a></li>
