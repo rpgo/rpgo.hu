7a1e57ceb9c5707cb4e2c2408844b65fbff99137
Lily Belle <tolilybelle@gmail.com>
2015-02-22 19:03:03 +0100

    Change order of links in the world menu

--- resources/views/common/nav/world.blade.php
+++ resources/views/common/nav/world.blade.php
@@ -1,6 +1,6 @@
+<li><a href="{{route('location.show', [$world, $world->rootlocation()])}}">{{$world->rootlocation()->name}}</a></li>
 @if($member)
     <li><a href="{{route('member.index', $world)}}">{{trans('member.index.menu')}}</a></li>
 @else
     <li><a href="{{route('member.create', $world)}}">{{trans('member.create.menu')}}</a></li>
 @endif
-<li><a href="{{route('location.show', [$world, $world->rootlocation()])}}">{{$world->rootlocation()->name}}</a></li>
