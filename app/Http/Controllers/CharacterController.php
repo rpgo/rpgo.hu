aa1bee6bf007554b9c3552ce8431f8488e12f8e1
Lily Belle <tolilybelle@gmail.com>
2015-02-22 19:20:51 +0100

    Add methods to CharacterController

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 
 class CharacterController extends Controller {
 
-	//
+	public function index()
+    {
+        return view('character.index');
+    }
+
+    public function create()
+    {
+        return view('character.create');
+    }
+
+    public function show($character)
+    {
+        return view('character.show');
+    }
 
 }
