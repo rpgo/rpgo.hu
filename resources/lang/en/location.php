a3e7a3e027b123a3dc47309cc6e9a3258a574a6c
Lily Belle <tolilybelle@gmail.com>
2015-02-22 08:42:54 +0100

    Translate location create and edit views

--- resources/lang/en/location.php
+++ resources/lang/en/location.php
@@ -8,4 +8,17 @@ return [
         'edit' => 'Edit location',
         'sublocations' => 'Sublocations',
     ],
+    'edit' => [
+        'title' => 'Edit location',
+        'heading' => 'Edit location here',
+        'submit' => 'Save',
+    ],
+    'create' => [
+        'title' => 'Create new location',
+        'heading' => 'Create new location here',
+        'submit' => 'Create',
+    ],
+    'form' => [
+        'name' => 'Name'
+    ],
 ];
--- resources/lang/hu/location.php
+++ resources/lang/hu/location.php
@@ -8,4 +8,17 @@ return [
         'edit' => 'Helyszín szerkesztése',
         'sublocations' => 'Alhelyszínek',
     ],
+    'edit' => [
+        'title' => 'Helyszín szerkesztése',
+        'heading' => 'Helyszín szerkesztése itt',
+        'submit' => 'Mentés',
+    ],
+    'create' => [
+        'title' => 'Új helyszín létrehozása',
+        'heading' => 'Új helyszín létrehozása itt',
+        'submit' => 'Mehet',
+    ],
+    'form' => [
+        'name' => 'Név'
+    ],
 ];
