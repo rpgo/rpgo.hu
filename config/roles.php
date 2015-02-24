f1ffaea31dc79421aa57905ab8632668281a9c8b
Lily Belle <tolilybelle@gmail.com>
2015-02-24 08:45:34 +0100

    Fix config (can not use locales in config)

--- config/roles.php
+++ config/roles.php
@@ -2,41 +2,11 @@
 
 return [
     'common' => [
-        'admin' => [
-            'name_plural' => trans('role.common.admin.name_plural'),
-            'name_singular' => trans('role.common.admin.name_singular'),
-            'description' => trans('role.common.admin.description'),
-            'custom' => false,
-        ],
-        'player' => [
-            'name_plural' => trans('role.common.player.name_plural'),
-            'name_singular' => trans('role.common.player.name_singular'),
-            'description' => trans('role.common.player.description'),
-            'custom' => false,
-        ],
-        'storyteller' => [
-            'name_plural' => trans('role.common.storyteller.name_plural'),
-            'name_singular' => trans('role.common.storyteller.name_singular'),
-            'description' => trans('role.common.storyteller.description'),
-            'custom' => false,
-        ],
-        'reader' => [
-            'name_plural' => trans('role.common.reader.name_plural'),
-            'name_singular' => trans('role.common.reader.name_singular'),
-            'description' => trans('role.common.reader.description'),
-            'custom' => false,
-        ],
-        'stranger' => [
-            'name_plural' => trans('role.common.stranger.name_plural'),
-            'name_singular' => trans('role.common.stranger.name_singular'),
-            'description' => trans('role.common.stranger.description'),
-            'custom' => false,
-        ],
-        'anonymous' => [
-            'name_plural' => trans('role.common.anonymous.name_plural'),
-            'name_singular' => trans('role.common.anonymous.name_singular'),
-            'description' => trans('role.common.anonymous.description'),
-            'custom' => false,
-        ],
+        'admin',
+        'player',
+        'storyteller',
+        'reader',
+        'stranger',
+        'anonymous',
     ],
 ];
