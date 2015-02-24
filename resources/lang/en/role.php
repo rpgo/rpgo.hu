4a637f3958d0ba82882b98cb8a5b0a985191431b
Lily Belle <tolilybelle@gmail.com>
2015-02-24 08:36:23 +0100

    Add configuration of default roles of a world

---
+++ resources/lang/en/role.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'common' => [
+        'admin' => [
+            'name_plural' => 'Administrators',
+            'name_singular' => 'Administrator',
+            'description' => 'Active members of the world, who have access to everywhere.',
+        ],
+        'player' => [
+            'name_plural' => 'Players',
+            'name_singular' => 'Player',
+            'description' => 'Active members of the world, who created at least one player character.',
+        ],
+        'storyteller' => [
+            'name_plural' => 'Storytellers',
+            'name_singular' => 'Storyteller',
+            'description' => 'Active members of the world, who created at least one storyteller character.',
+        ],
+        'reader' => [
+            'name_plural' => 'Readers',
+            'name_singular' => 'Reader',
+            'description' => 'Active members of the world, who don\'t take part in the game, just observe it.',
+        ],
+        'stranger' => [
+            'name_plural' => 'Guests',
+            'name_singular' => 'Guest',
+            'description' => 'Registered users of rpgo.hu, who have not joined this world yet.',
+        ],
+        'anonymous' => [
+            'name_plural' => 'Anonymous',
+            'name_singular' => 'Anonymous',
+            'description' => 'Unregistered visitors of rpgo.hu.',
+        ],
+    ],
+];
---
+++ resources/lang/hu/role.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'common' => [
+        'admin' => [
+            'name_plural' => 'Adminisztrátorok',
+            'name_singular' => 'Adminisztrátor',
+            'description' => 'A világ olyan aktív tagjai, akiknek minden felülethez van hozzáférésük az oldalon.',
+        ],
+        'player' => [
+            'name_plural' => 'Játékosok',
+            'name_singular' => 'Játékos',
+            'description' => 'A világ olyan aktív tagjai, akik már készítettek legalább egy játékos karaktert.',
+        ],
+        'storyteller' => [
+            'name_plural' => 'Mesélők',
+            'name_singular' => 'Mesélő',
+            'description' => 'A világ olyan aktív tagjai, akik már készítettek legalább egy mesélő karaktert.',
+        ],
+        'reader' => [
+            'name_plural' => 'Olvasók',
+            'name_singular' => 'Olvasó',
+            'description' => 'A világ olyan aktív tagjai, akik nem vesznek részt a játékban, csak figyelemmel követik az eseményeket.',
+        ],
+        'stranger' => [
+            'name_plural' => 'Vendégek',
+            'name_singular' => 'Vendég',
+            'description' => 'Az rpgo.hu regisztrált felhasználói, akik még nem csatlakoztak a világhoz.',
+        ],
+        'anonymous' => [
+            'name_plural' => 'Idegenek',
+            'name_singular' => 'Idegen',
+            'description' => 'Az rpgo.hu-ra nem regisztrált felhasználók, akiknek a kiléte ismeretlen még.',
+        ],
+    ],
+];
