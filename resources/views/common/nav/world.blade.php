2e3aef5872bc0c198f2aee0d04d022c4701aa05b
Lily Belle <tolilybelle@gmail.com>
2015-02-25 07:59:05 +0100

    Switch order of world menu items

--- resources/views/common/nav/world.blade.php
+++ resources/views/common/nav/world.blade.php
@@ -1,8 +1,8 @@
 <li><a href="{{route('location.show', [$world, $world->rootlocation()])}}">{{$world->rootlocation()->name}}</a></li>
+<li><a href="{{route('character.index', $world)}}">@lang('character.index.menu')</a></li>
 @if($member)
     <li><a href="{{route('member.index', $world)}}">@lang('member.index.menu')</a></li>
 @else
     <li><a href="{{route('member.create', $world)}}">@lang('member.create.menu')</a></li>
 @endif
-<li><a href="{{route('character.index', $world)}}">@lang('character.index.menu')</a></li>
 {{-- <li><a href="{{route('character.create', $world)}}">@lang('character.create.menu')</a></li> --}}
