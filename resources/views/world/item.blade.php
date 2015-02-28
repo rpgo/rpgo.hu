eb3ccb911b07c7b83d64563cc7176b27fa9a0e9e
Lily Belle <tolilybelle@gmail.com>
2015-02-28 13:17:58 +0100

    Work on world index styling

--- resources/views/world/item.blade.php
+++ resources/views/world/item.blade.php
@@ -4,18 +4,16 @@
             <img class="media-object" src="{{asset('images/previews/' . $item['slug'] . '.jpg')}}">
         </a>
         <div class="media-body">
-            <h4 class="media-heading"><a href="{{route('world.main', $item)}}">{{$item['name']}} ({{$item['brand']}})</a></h4>
+            <h5 class="media-heading"><a href="{{route('world.main', $item)}}">{{$item['name']}} <span class="hidden-xs">({{$item['brand']}})</span></a></h5>
             {{--<p class="text-right">{{trans('world.creator.title') . ': ' . $item->creator->name}}</p>--}}
-            <ul class="list-unstyled list-inline">
-                <li><span>{{trans('world.item.members') . ': ' . $item->members()->count()}} </span></li>
+            <ul class="list-unstyled list-inline list-group">
+                <li class="list-group-item"><span>{{trans('world.item.members') . ': ' . $item->members()->count()}} </span></li>
                 @foreach($item['types'] as $type)
-                    <li class="hidden-xs">|</li>
-                    <li class="hidden-xs">{{$type['name_group']}}: {{$item->members()->ofType($type)->count()}}</li>
+                    <li class="hidden-xs list-group-item">{{$type['name_group']}}: {{$item->members()->ofType($type)->count()}}</li>
                 @endforeach
             </ul>
             <ul class="list-inline list-unstyled">
-                <li><span>{{trans('world.item.locations') . ': ' . $item->rootlocation()->sublocations()->count()}} </span></li>
-                <li>|</li>
+                {{--<li><span>{{trans('world.item.locations') . ': ' . $item->rootlocation()->sublocations()->count()}} </span></li><li>|</li>--}}
                 <li>Link: <a href="{{route('world.main', compact('world'))}}">{{$item['slug'] . '.' . env('APP_DOMAIN')}}</a></li>
             </ul>
             <p>{{$item['motto']}} <a href="{{route('world.show', $item)}}">Bővebben...</a></p>
