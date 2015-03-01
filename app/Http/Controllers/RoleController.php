3443015f2025cb35b0d3c232bdd7eddbd501108c
Lily Belle <tolilybelle@gmail.com>
2015-03-01 08:43:44 +0100

    Add controller for roles

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,20 @@
+<?php namespace Rpgo\Http\Controllers;
+
+class RoleController extends Controller {
+
+	public function index()
+    {
+        return 'index';
+    }
+
+    public function store()
+    {
+        return 'store';
+    }
+
+    public function show()
+    {
+        return 'show';
+    }
+
+}
