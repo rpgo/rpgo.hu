2e6859ebad338deff1d1852459b99dac5ee9542c
Lily Belle <tolilybelle@gmail.com>
2015-03-01 09:14:55 +0100

    Add roles list to role index

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -1,10 +1,17 @@
 <?php namespace Rpgo\Http\Controllers;
 
+use Rpgo\Models\Role;
+use Rpgo\Rpgo;
+
 class RoleController extends Controller {
 
-	public function index()
+	public function index(Rpgo $rpgo)
     {
-        return view('role.index');
+        $world = $rpgo->world();
+
+        $roles = Role::ofWorld($world)->get();
+
+        return view('role.index')->with(compact('roles'));
     }
 
     public function store()
--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -3,5 +3,17 @@
 @section('subtitle') @lang('role.index.title') @endsection
 
 @section('subcontent')
+    <table class="table table-striped table-bordered">
+        <tr>
+            <th>@lang('role.name.info')</th>
+            <th>Kijelölés</th>
+        </tr>
+        @foreach($roles as $role)
+            <tr>
+                <td><a href="{{route('role.show', [$world, $role])}}">{{$role['name_solo']}}</a></td>
+                <td></td>
+            </tr>
+        @endforeach
 
+    </table>
 @endsection
