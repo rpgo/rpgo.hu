df17fcc098458778555a180a86cc2cb1ed7c2fef
Lily Belle <tolilybelle@gmail.com>
2015-03-01 07:42:04 +0100

    Add placeholder for character creation link

--- resources/views/common/nav/member.blade.php
+++ resources/views/common/nav/member.blade.php
@@ -1,4 +1,7 @@
 <li class="dropdown">
+    <a href="#">Karakteralkotás</a>
+</li>
+<li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ $member->name }} <span class="caret"></span></a>
     <ul class="dropdown-menu" role="menu">
         <li><a href="{{route('member.show', [$world, $member])}}">@lang('member.show.menu')</a></li>
