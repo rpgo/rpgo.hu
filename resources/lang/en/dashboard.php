6988f12512946899174d94b2f2aa1e66f8dc5f52
Lily Belle <tolilybelle@gmail.com>
2015-03-01 07:47:19 +0100

    Add translations for dashboard

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'main' => [
+        'menu' => 'Dashboard',
+        'sidebar' => 'Overview',
+        'title' => 'Overview',
+    ],
+];
---
+++ resources/lang/hu/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Irányítópult',
+    'main' => [
+        'menu' => 'Irányítópult',
+        'sidebar' => 'Áttekintés',
+        'title' => 'Áttekintés',
+    ],
+];
