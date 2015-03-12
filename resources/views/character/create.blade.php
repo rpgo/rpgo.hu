35f33a7da5c0ae67b08a2cf4b0a0e5654a14a5c7
Lily Belle <tolilybelle@gmail.com>
2015-03-12 08:14:56 +0100

    Add links to character creation

--- resources/views/character/create.blade.php
+++ resources/views/character/create.blade.php
@@ -3,5 +3,12 @@
 @section('title') Character Creation @endsection
 
 @section('content')
-    Character Creation
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-offset-4 col-xs-4">
+                <p><a href="#" type="button" class="btn btn-primary btn-lg">Játékos karakter</a></p>
+                <p><a href="#" type="button" class="btn btn-primary btn-lg">Mesélő karakter</a></p>
+            </div>
+        </div>
+    </div>
 @endsection
