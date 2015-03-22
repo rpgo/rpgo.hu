b2311ce1b329e29f258c03f284885c4d322d2866
Lily Belle <tolilybelle@gmail.com>
2015-03-22 14:14:30 +0100

    Translate settings

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'dashboard' => [
+        'menu' => 'Settings',
+    ],
+    'form' => [
+        'language' => 'Language',
+    ],
+    'edit' => [
+        'title' => 'Settings',
+        'header' => 'Settings',
+        'submit' => 'Save',
+    ],
+];
---
+++ resources/lang/hu/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'dashboard' => [
+        'menu' => 'Beállítások',
+    ],
+    'form' => [
+        'language' => 'Nyelv',
+    ],
+    'edit' => [
+        'title' => 'Beállítások',
+        'header' => 'Beállítások',
+        'submit' => 'Mentés',
+    ],
+];
