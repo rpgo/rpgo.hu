1721ec2670c15f4e869e8077b31162a7bce997be
Lily Belle <tolilybelle@gmail.com>
2015-03-22 13:45:26 +0100

    Group nav items together

--- resources/views/common/nav/world.blade.php
+++ resources/views/common/nav/world.blade.php
@@ -1,11 +1,15 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Játéktér <span class="caret"></a>
+    <ul class="dropdown-menu" role="menu">
+        <li><a href="{{route('location.show', [$world, $world->rootlocation()])}}">{{$world->rootlocation()->name}}</a></li>
+        <li><a href="{{route('character.index', $world)}}">@lang('character.index.menu')</a></li>
+    </ul>
+</li>
 @if(can('use.control'))
     <li><a href="{{route('dashboard.main', [$world])}}">@lang('dashboard.main.menu')</a></li>
 @endif
-<li><a href="{{route('location.show', [$world, $world->rootlocation()])}}">{{$world->rootlocation()->name}}</a></li>
-<li><a href="{{route('character.index', $world)}}">@lang('character.index.menu')</a></li>
 @if($member)
     <li><a href="{{route('member.index', $world)}}">@lang('member.index.menu')</a></li>
 @else
     <li><a href="{{route('member.create', $world)}}">@lang('member.create.menu')</a></li>
 @endif
-{{-- <li><a href="{{route('character.create', $world)}}">@lang('character.create.menu')</a></li> --}}
