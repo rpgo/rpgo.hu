bde136188fdb0c02fa296e69934b85685cf32511
Lily Belle <tolilybelle@gmail.com>
2015-03-22 03:53:28 +0100

    Remove unnecessary methods in SettingsController

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -1,55 +1,11 @@
 <?php namespace Rpgo\Http\Controllers;
 
-use Rpgo\Http\Requests;
-use Rpgo\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SettingsController extends Controller {
 
 	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
+	 * Show the form for editing the world settings.
 	 *
 	 * @param  int  $id
 	 * @return Response
@@ -60,23 +16,12 @@ class SettingsController extends Controller {
 	}
 
 	/**
-	 * Update the specified resource in storage.
+	 * Update the settings in storage.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
 	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
 	{
 		//
 	}
