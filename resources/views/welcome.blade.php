1f8b723065406c5b17271e59fc8a68b1304544bc
Lily Belle <tolilybelle@gmail.com>
2015-03-22 06:21:33 +0100

    Tweak the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,25 +6,24 @@
         <header class="jumbotron hero-spacer">
             <h1>RPGO.hu <span class="hidden-xs small">- A szerepjátékok otthona!</span> </h1>
             <p>Ahol az online szerepjáték könnyed élmény! Garanciát biztosítunk a minőségre az frpg játékosok, mesélők és világalkotók számára egyaránt.</p>
-            <p><a href="{{route('user.create')}}" class="btn btn-primary btn-large">Erre kíváncsi vagyok!</a>
-            </p>
+            <p><a href="{{route('user.create')}}" class="btn btn-primary btn-large">Hiszem, ha látom!</a></p>
         </header>
 
         <div class="row">
             <div class="col-sm-4">
                 <h2>Játékosoknak</h2>
                 <p>Férj hozzá egyetlen regisztrációval és bejelentkezéssel az összes világon meglevő összes karakteredhez, kalandodhoz, üzenetedhez, valamint minden más téged érintő hírhez.</p>
-                <p><a href="{{route('user.create')}}" class="btn btn-primary btn-large">Erre kíváncsi vagyok!</a>
+                <p><a href="{{route('user.create')}}" class="btn btn-default btn-large">Mindig ilyen játékra vágytam!</a></p>
             </div>
             <div class="col-sm-4">
                 <h2>Mesélőknek</h2>
                 <p>Tedd visszakereshetővé a kalandjaidat! Emeld az NJK-id hatását önálló profillal! Kezeld a poszt-sorrendet, az inaktivitást, a felszereléseket könnyedén.</p>
-                <p><a href="{{route('user.create')}}" class="btn btn-primary btn-large">Erre kíváncsi vagyok!</a>
+                <p><a href="{{route('user.create')}}" class="btn btn-default btn-large">Végre megvalósíthatom a kalandom!</a></p>
             </div>
             <div class="col-sm-4">
                 <h2>Világalkotóknak</h2>
                 <p>Készíts valódi világot sima fórum helyett! Gyakorolj könnyedén teljes befolyást egyszerű beállításokkal a világod működésére, kinézetére és tartalmára.</p>
-                <p><a href="{{route('user.create')}}" class="btn btn-primary btn-large">Erre kíváncsi vagyok!</a>
+                <p><a href="{{route('user.create')}}" class="btn btn-default btn-large">A világomnak itt a helye!</a></p>
             </div>
         </div>
     </div>
