f6388dfed3591db9f67e98224c27efdb51836e23
Lily Belle <tolilybelle@gmail.com>
2015-03-22 04:47:54 +0100

    Implement settings update

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -1,8 +1,25 @@
 <?php namespace Rpgo\Http\Controllers;
 
 use Illuminate\Http\Response;
+use Rpgo\Http\Requests\EditSettings;
+use Rpgo\Models\Settings;
+
 
 class SettingsController extends Controller {
+
+	/**
+	 * @var Settings
+	 */
+	private $settings;
+
+	/**
+	 * SettingsController constructor.
+	 */
+	public function __construct()
+	{
+		$world = $this->world();
+		$this->settings = $world['settings'];
+	}
 
 	/**
 	 * Show the form for editing the world settings.
@@ -11,8 +28,7 @@ class SettingsController extends Controller {
 	 */
 	public function edit()
 	{
-		$world = $this->world();
-		$settings = $world['settings'];
+		$settings = $this->settings;
 
 		return view('settings.edit')->with(compact('settings'));
 	}
@@ -22,9 +38,27 @@ class SettingsController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function update()
+	public function update(EditSettings $request)
 	{
-		//
+		$settings = $this->settings;
+
+		$this->updateLanguage($request, $settings);
+
+		$settings->save();
+
+		return view('settings.edit')->with(compact('settings'));
+	}
+
+	/**
+	 * @param EditSettings $request
+	 * @param Settings $settings
+	 * @return mixed
+     */
+	
+	private function updateLanguage(EditSettings $request, $settings)
+	{
+		if ($request->has('language'))
+			$settings['language'] = $request->get('language');
 	}
 
 }
---
+++ app/Http/Requests/EditSettings.php
@@ -0,0 +1,29 @@
+<?php namespace Rpgo\Http\Requests;
+
+use Rpgo\Http\Requests\Request;
+
+class EditSettings extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'language' => ['in:hu,en'],
+		];
+	}
+
+}
