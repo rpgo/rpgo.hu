3939ae60c8753bdfdfef9588b7914efecd1cf578
Lily Belle <tolilybelle@gmail.com>
2015-03-22 07:59:23 +0100

    Halve the world main content

--- resources/views/world/main.blade.php
+++ resources/views/world/main.blade.php
@@ -4,19 +4,28 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="well">
-            <ul class="list-inline">
-                <li>@lang('world.main.online_characters'):</li>
-                @foreach($onlineCharacters as $character)
-                    <li><a href="{{route('character.show', [$world, $character])}}">{{$character['name']}}</a></li>
-                @endforeach
-            </ul>
-            <ul class="list-inline">
-                <li>@lang('world.main.online_members'):</li>
-                @foreach($onlineMembers as $online_member)
-                    <li><a href="{{route('member.show', [$world, $online_member])}}">{{$online_member['name']}}</a></li>
-                @endforeach
-            </ul>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="well">
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="well">
+                    <ul class="list-inline">
+                        <li>@lang('world.main.online_characters'):</li>
+                        @foreach($onlineCharacters as $character)
+                            <li><a href="{{route('character.show', [$world, $character])}}">{{$character['name']}}</a></li>
+                        @endforeach
+                    </ul>
+                    <ul class="list-inline">
+                        <li>@lang('world.main.online_members'):</li>
+                        @foreach($onlineMembers as $online_member)
+                            <li><a href="{{route('member.show', [$world, $online_member])}}">{{$online_member['name']}}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
