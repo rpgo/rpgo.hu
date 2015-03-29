54c23c6c4c19b223e695f2c74005554e25dd05fb
Lily Belle <tolilybelle@gmail.com>
2015-03-29 07:48:13 +0200

    Link header to main page of world

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,7 @@
 @if($world)
 <div style="margin-top:-20px; margin-bottom: 20px;">
-    <img src="http://placehold.it/1000x150" class="img-responsive center-block"/>
+    <a href="{{route('world.main', [$world])}}">
+        <img src="http://placehold.it/1000x150" class="img-responsive center-block"/>
+    </a>
 </div>
 @endif
